e8a4824a09bfa3b4bcac51cd8c56871eaf32123f
Deveshsaini07 <devesh07saini@gmail.com>
2025-06-29 23:05:50 +0530

    updated backedn url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:3000'
+      '/api':'https://todo-api.parveshsaini.com'
     }
   },
   plugins: [
